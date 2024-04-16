9d497c4b1d1b6528fd5e2fc0365b345188a9d986
Oron Feldman <oronfeldman@gamil.com>
2024-04-16 23:45:21 +0300

    Adding inference flask file with needed requirements.txt file

---
+++ aws_inference.py
@@ -0,0 +1,24 @@
+from tensorflow.keras.models import load_model
+from flask import Flask
+from flask import request
+import numpy as np
+import json
+
+
+app = Flask(__name__)
+
+
+@app.route('/predict_album', methods=['POST'])
+def inference_func():
+    image_data = request.files['image'].read()
+    img = np.frombuffer(image_data, dtype=np.float32).reshape((1, 299, 299, 3))
+    classes_dict = json.loads(request.files['classes_dict'].read())
+    names_dict = json.loads(request.files['names_dict'].read())
+    predictions = model.predict(img)
+    predicted_label = classes_dict[str(np.argmax(predictions, axis=1)[0])]
+    album_name = names_dict[str(predicted_label)]
+    return album_name
+
+
+model = load_model(r'C:\Users\oronf\Documents\PythonProjects\FinalProjectDS\testing\InceptionResNetV2_model')
+app.run(host='0.0.0.0', port=8080)
